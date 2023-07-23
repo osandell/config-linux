ef1c22e2c3bb7ad04e6b7bed1271d37c5dfa8f4c
Olof Sandell <83674246+osandell@users.noreply.github.com>
2023-07-23 11:19:54 +0200

    add script to switch to ext monitors

---
+++ home/olof/.config/kmonad/swap-keyboards.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define the configuration file path
+config_file=~/.config/kmonad/defcfg.part
+
+# Define the event id file path
+event_id_file=$(dirname "$0")/bt-keyboard-id.txt
+
+# Read the event id from the file
+event_id=$(cat "$event_id_file")
+
+# Check if the BT keyboard is connected
+if ls $event_id >/dev/null 2>&1; then
+    # If the device exists, check if the line is commented in the file
+    if grep -q ";; input (device-file \"$event_id\")" $config_file; then
+        # If so we activate BT keyboard by uncommenting both lines then comment internal kb line
+        sed -i -e "s|;; input (device-file \"$event_id\")|input (device-file \"$event_id\")|" -e 's/input (device-file "\/dev\/input\/by-id\/usb-Apple_Inc._Apple_Internal_Keyboard___Trackpad_FM710660210HYYKCP+RVZ-if01-event-kbd")/;; &/' $config_file
+        
+        # Also move workspaces to external monitors
+        ~/.config/kmonad/swap-monitors.sh
+    else
+        # If not it means that the BT keyboard is active so we swap to internal
+        sed -i -e "s|input (device-file \"$event_id\")|;; input (device-file \"$event_id\")|" -e 's/;; input (device-file "\/dev\/input\/by-id\/usb-Apple_Inc._Apple_Internal_Keyboard___Trackpad_FM710660210HYYKCP+RVZ-if01-event-kbd")/input (device-file "\/dev\/input\/by-id\/usb-Apple_Inc._Apple_Internal_Keyboard___Trackpad_FM710660210HYYKCP+RVZ-if01-event-kbd")/' $config_file
+    fi
+else
+    # If BT keyboard is disconnected we make sure to activate internal
+    sed -i -e "s|;; input (device-file \"$event_id\")|input (device-file \"$event_id\")|" -e 's/input (device-file "\/dev\/input\/by-id\/usb-Apple_Inc._Apple_Internal_Keyboard___Trackpad_FM710660210HYYKCP+RVZ-if01-event-kbd")/;; &/' $config_file
+fi
---
+++ home/olof/.config/kmonad/swap-monitors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get ID of left monitor
+monitor_id=$(hyprctl monitors | awk '/S2721QS 8YTLM43/' | awk -F'[()]' '{print $2}')
+
+# Array of workspace numbers
+workspaces=(1 2 4 5 6 7 8 9 0)
+
+# Move each workspace to the monitor
+for workspace in ${workspaces[@]}; do
+  hyprctl dispatch moveworkspacetomonitor "$workspace $monitor_id"
+done
+
+# Get ID of right monitor
+monitor_id=$(hyprctl monitors | awk '/S2721QS 8BJB513/' | awk -F'[()]' '{print $2}')
+
+# Move 'Dev' browser to right monitor
+hyprctl dispatch moveworkspacetomonitor "3 $monitor_id"
